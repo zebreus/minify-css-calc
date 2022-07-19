1f2e077b9a167578d5a99cd1c915e7fb484ade33
Zebreus <lennarteichhorn@googlemail.com>
2022-07-19 14:37:42 +0200

    Adjust optimization runner to allow nested stages

--- src/optimizeAst.ts
+++ src/optimizeAst.ts
@@ -8,34 +8,46 @@ type OptimizerStorage = {
 
 type OptimizerFunction = (node: Node, storage?: OptimizerStorage) => Node;
 
+type OptimizerStage = Array<OptimizerStage> | OptimizerFunction;
+
 /** Applies each stage of optimizations until they dont mutate the ast */
 export const optimizeAst = (
   ast: Node,
-  stages: Array<Array<OptimizerFunction>>,
+  stages: OptimizerStage,
   storage: OptimizerStorage = {}
 ): Node => {
   if (stages.length === 0) {
     return ast;
   }
-  const [currentStage, ...nextStages] = stages;
+  // const [currentStage, ...nextStages] = stages;
 
-  const previousString = JSON.stringify(ast);
-  const optimizedAst = processStage(ast, currentStage, storage);
-  const optimizedString = JSON.stringify(optimizedAst);
+  // const previousString = JSON.stringify(ast);
+  // const optimizedAst =
+  // const optimizedString = JSON.stringify(optimizedAst);
   // debugNode(optimizedAst);
 
-  return optimizeAst(
-    optimizedAst,
-    previousString === optimizedString ? nextStages : stages,
-    storage
-  );
+  return processStage(ast, stages, storage);
 };
 
 /** Apply a stage */
 const processStage = (
   ast: Node,
-  stages: Array<OptimizerFunction>,
+  stage: OptimizerStage,
   storage: OptimizerStorage
-) => {
-  return stages.reduce((ast, stage) => stage(ast, storage), ast);
+): Node => {
+  if (Array.isArray(stage)) {
+    const [currentStage, ...nextStages] = stage;
+    const previousString = JSON.stringify(ast);
+    const optimizedAst = stage.reduce(
+      (ast, stage) => processStage(ast, stage, storage),
+      ast
+    );
+    const optimizedString = JSON.stringify(optimizedAst);
+
+    return previousString === optimizedString
+      ? optimizedAst
+      : processStage(optimizedAst, stage, storage);
+  } else {
+    return stage(ast, storage);
+  }
 };
