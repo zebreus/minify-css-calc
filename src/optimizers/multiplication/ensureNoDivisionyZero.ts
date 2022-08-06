447d2dfeacbd3aa31a4bcad8a9f4d7221ed98231
Zebreus <lennarteichhorn@googlemail.com>
2022-08-06 16:15:19 +0200

    Move zero division chek to seperate stage

---
+++ src/optimizers/multiplication/ensureNoDivisionyZero.ts
@@ -0,0 +1,27 @@
+import { debugNode, stringifyNode } from "stringifyNode";
+import { MultiplicationNode, Node, ValueNode } from "../../parseCalc";
+import { visitor } from "../../visitor";
+
+export const ensureNoDivisionByZero = (node: Node) => {
+  return visitor(
+    node,
+    () => true,
+    (node: Node) => {
+      if (node.type !== "multiplication") {
+        return node;
+      }
+
+      node.values.forEach((value) => {
+        if (
+          value.value.type === "value" &&
+          value.operation === "/" &&
+          value.value.value === 0
+        ) {
+          throw new Error(`Division by zero in ${stringifyNode(node)}`);
+        }
+      });
+
+      return node;
+    }
+  );
+};
